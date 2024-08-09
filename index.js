1d25427d6cb6b0def0ec5de9e1b3395178b9b6bf
Luke Balmer <lukembalmer@gmail.com>
2024-08-09 15:59:08 -0500

    8/9/24

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //Luke Balmer
 
 //onclick="subscribe();" class="js-subscribe"
+function mobileResponsiveNav() {
+    var x = document.getElementById("myHeader");
+    if (x.className === "header-nav-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "header-nav-container";
+    }
+}
 function subscribe() {
     const subButtonElement = document.querySelector('.js-subscribe-button');
     if(subButtonElement.innerHTML === 'I Love Iris Button') {
